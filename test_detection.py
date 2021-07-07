ef17b68013d39638c316b1896fcc2153c603d705
ianzih <abc021789@gmail.com>
2021-07-07 16:03:45 +0800

    first commit

---
+++ test_detection.py
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+from keras import backend as K
+from keras.models import load_model
+from keras.preprocessing import image
+=======
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing import image
+>>>>>>> test
+import sys
+import numpy as np
+
+# 從參數讀取圖檔路徑
+files = sys.argv[1:]
+
+# 載入訓練好的模型
+<<<<<<< HEAD
+net = load_model('model-resnet50-final.h5')
+=======
+net = load_model('model-resnet101-final.h5')
+>>>>>>> test
+
+cls_list = ['cats', 'dogs']
+
+# 辨識每一張圖
+for f in files:
+    img = image.load_img(f, target_size=(224, 224))
+    if img is None:
+        continue
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis = 0)
+    pred = net.predict(x)[0]
+    top_inds = pred.argsort()[::-1][:5]
+    print(f)
+    for i in top_inds:
+        print('    {:.3f}  {}'.format(pred[i], cls_list[i]))
